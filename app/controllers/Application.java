771dec0d3b45357b76c5663ad7f63c517ae22b6b
rcernaUTC <romelcerna1@gmail.com>
2015-07-29 17:18:48 -0500

    proyectdis 8
    implemente una funcionalidad chat chequeen mijines

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,15 +1,34 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Required;
 import play.mvc.*;
 
 import java.util.*;
+
 import models.*;
 
 public class Application extends Controller {
 
     public static void index() {
         render();
+    }
+
+    public static void chat() {
+        render();
+    }
+    
+    public static void enterDemo(@Required String user, @Required String demo) {        
+        if(validation.hasErrors()) {
+            flash.error("Please choose a nick name and the demonstration type…");
+            chat();
+        }
+        
+        // Dispatch to the demonstration        
+        if(demo.equals("refresh")) {
+        	Refresh.chat(user);
+        }
+     
     }
 
 }
